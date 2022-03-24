ceed70a6d7f307c5143de9c8ce5f949658f1704a
Bruno Inácio <brunoinacio3000@hotmail.com>
2022-03-24 12:03:09 -0300

    Passarinho voa, limite horizontal, camera segue

--- Assets/Scripts/ArrastaProjetil.cs
+++ Assets/Scripts/ArrastaProjetil.cs
@@ -2,49 +2,120 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrastaProjetil : MonoBehaviour
-{
+public class ArrastaProjetil : MonoBehaviour {
+
     bool clicou;
+
     public float esticadaMaxima = 3.0f;
     float esticadaMaximaQuadrada;
-    SpringJoint2D  mola;
+
+    SpringJoint2D mola;
     Rigidbody2D meuRigidbody;
+
     Transform estilingue;
     Ray raioParaMouse;
-    // Start is called before the first frame update
-    void Start()
+    Ray raioEstilingueFrente;
+
+    CircleCollider2D colisor;
+    float medidaCirculo;
+
+    Vector2 velocidadeAnterior;
+
+    public LineRenderer linhaFrente;
+    public LineRenderer linhaTras;
+
+    private void Awake()
     {
         mola = GetComponent<SpringJoint2D>();
+        meuRigidbody = GetComponent<Rigidbody2D>();
+        colisor = GetComponent<CircleCollider2D>();
+    }
+
+    void Start () {
         estilingue = mola.connectedBody.transform;
         esticadaMaximaQuadrada = esticadaMaxima * esticadaMaxima;
         raioParaMouse = new Ray(estilingue.position, Vector3.zero);
-    }
+        raioEstilingueFrente = new Ray(linhaFrente.transform.position, Vector3.zero);
 
-    // Update is called once per frame
-    void Update()
+        medidaCirculo = colisor.radius;
+
+        ConfiguraLinha();
+	}
+	
+    void ConfiguraLinha()
     {
-        if(clicou==true){
+        linhaFrente.SetPosition(0, linhaFrente.transform.position + Vector3.forward * -0.03f);
+        linhaTras.SetPosition(0, linhaTras.transform.position + Vector3.forward * -0.01f);
+
+        linhaFrente.sortingLayerName = "Frente;";
+        linhaTras.sortingLayerName = "Frente;";
+
+        linhaFrente.sortingOrder = 3;
+        linhaTras.sortingOrder = 1;
+    }
+
+    void AtualizaLinha()
+    {
+        Vector2 estilingueParaProgetil = transform.position - linhaFrente.transform.position;
+        raioEstilingueFrente.direction = estilingueParaProgetil;
+        Vector3 pontoDeAmarra = raioEstilingueFrente.GetPoint(estilingueParaProgetil.magnitude + medidaCirculo);
+
+        pontoDeAmarra.z = -0.03f;
+        linhaFrente.SetPosition(1, pontoDeAmarra);
+        pontoDeAmarra.z = -0.01f;
+        linhaTras.SetPosition(1, pontoDeAmarra);
+    }
+
+	void Update ()
+    {
+        if (clicou == true)
             Arrastar();
+
+        if(mola != null)
+        {
+            if (!meuRigidbody.isKinematic && velocidadeAnterior.sqrMagnitude > meuRigidbody.velocity.sqrMagnitude)
+            {
+                Destroy(mola);
+                meuRigidbody.velocity = velocidadeAnterior;
+            }
+
+            if (!clicou)
+                velocidadeAnterior = meuRigidbody.velocity;
+
+            AtualizaLinha();
         }
+        else
+        {
+
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        clicou = true;
+        mola.enabled = false;
     }
 
-    private void OnMouseDown() {
-        clicou= true;
-    }
-    private void OnMouseUp() {
-        clicou = false;    
+    private void OnMouseUp()
+    {
+        clicou = false;
+        mola.enabled = true;
+        meuRigidbody.bodyType = RigidbodyType2D.Dynamic;
     }
 
-    void Arrastar(){
-        Vector3 positionMouseMundo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 estilingueParaMouse = positionMouseMundo - estilingue.position;
+    void Arrastar()
+    {
+        Vector3 posicaoMouseMundo = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 estilingueParaMouse = posicaoMouseMundo - estilingue.position;
         
 
-        if(estilingueParaMouse.sqrMagnitude > esticadaMaxima){
+        if(estilingueParaMouse.sqrMagnitude > esticadaMaximaQuadrada)
+        {
             raioParaMouse.direction = estilingueParaMouse;
-            positionMouseMundo = raioParaMouse.GetPoint(esticadaMaxima);
+            posicaoMouseMundo = raioParaMouse.GetPoint(esticadaMaxima);
         }
-        positionMouseMundo.z = 0; 
-        transform.position = positionMouseMundo;
+
+        posicaoMouseMundo.z = -0.02f;
+        transform.position = posicaoMouseMundo;
     }
 }
---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform projetil;
+
+    public Transform limiteEsquerdo;
+
+    public Transform limiteDireito;
+
+    Vector3 novaPosicao;
+
+    // Update is called once per frame
+    void Update()
+    {
+        novaPosicao = transform.position;
+        novaPosicao.x = projetil.position.x;
+        novaPosicao.x = Mathf.Clamp(novaPosicao.x, limiteEsquerdo.position.x, limiteDireito.position.x);
+        transform.position = novaPosicao;
+    }
+}
